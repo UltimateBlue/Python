33201a59b5b167d2ac8d2fe6232da26e64bda88f
UltimateBlue <ultimatebluee@gmail.com>
2021-08-24 08:39:27 +0330

    final version - learning

--- 012-Caesar-cipher/main.py
+++ 012-Caesar-cipher/main.py
@@ -3,16 +3,26 @@ alphabets = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q'
 while True:
     codingType = input('for encoding type "e"\nfor decoding type "d"\nEnter your choise: ').lower()
     message = input('Write your message:').lower()
-    shiftNum = int(input('Enter your desired shift number: '))
+    shiftN = input('Enter your desired shift number: ')
+    if not shiftN.isnumeric():
+        print('Enter a valid number!')
+        continue
+
+    shiftNum = int(shiftN)
     coded = ''
 
     for c in message:
+        if not c.isalpha():
+            coded += c
+            continue
         i = alphabets.index(c)
         if codingType == 'e':
-            newIndex = i+shiftNum if i+shiftNum<26 else i+shiftNum-26
+            newIndex = i+(shiftNum%25) if i+(shiftNum%25)<26 else i+(shiftNum%25)-26
         elif codingType == 'd':
-            newIndex = i-shiftNum if i-shiftNum>=0 else 26 + i-shiftNum
-
+            newIndex = i-(shiftNum%25) if i-(shiftNum%25)>=0 else 26 + i-(shiftNum%25)
+        else:
+            print('you did not select any encryption!! ooops..')
+            break
         coded +=alphabets[newIndex]
     print(coded)
     contin = input('Do you want to continue? (Y/N) ').lower()
